6d3a07d12800e539ed4da4c193c57b01240492c4
Jesse Morris <jessmmorris7@gmail.com>
2022-03-28 19:34:01 +1100

    adding volume mount capabilities for dockerfile

--- docker/create_docker_container.sh
+++ docker/create_docker_container.sh
@@ -3,11 +3,22 @@
 CONTAINER_NAME=docker_vdo_slam
 CONTAINER_IMAGE_NAME=docker_vdo_slam
 
+### EDIT THIS TO WHEREVER YOU'RE STORING YOU DATA ###
+# folder should exist before you mount it
+LOCAL_DATA_FOLDER=/media/jesse/T7/datasets
+
+
+CONTAINER_DATA_FOLDER=/root/data
+
+echo "Mounting data folder (local) $LOCAL_DATA_FOLDER -> (container) $CONTAINER_DATA_FOLDER"
+
+
 docker create --privileged \
             --net=host \
             --pid=host \
             --name=$CONTAINER_NAME \
             -v /var/run/docker.sock:/var/run/docker.sock \
+            -v $LOCAL_DATA_FOLDER:$CONTAINER_DATA_FOLDER \
             -it \
             $CONTAINER_IMAGE_NAME \
             bash

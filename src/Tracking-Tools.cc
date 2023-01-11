7e6b2cd14f8060543c2616ad8dc8a5e344c490b9
root <jessmmorris7@gmail.com>
2023-01-11 06:50:04 +0000

    separating out functions

--- src/Tracking-Tools.cc
+++ src/Tracking-Tools.cc
@@ -1,4 +1,6 @@
 #include "Tracking-Tools.h"
+#include "utils/UtilsOpenCV.h"
+#include "UtilsGtsam.h"
 
 #include "Macros.h"
 #include "Types.h"
@@ -11,7 +13,7 @@
 
 
 namespace vdo {
-
+namespace tracking_tools {
 
 bool SortPairInt(const std::pair<int, int>& a, const std::pair<int, int>& b)
 {
@@ -181,11 +183,88 @@ void updateFrameMask(const Frame::Ptr& previous_frame, Frame::Ptr current_frame)
 
   VLOG(2) << "Updated mask poins - " << updated_mask_points;
 
+}
+
+MotionResult solvePnpOrMotion(const KeypointsCV& current_uv,
+                              const Landmarks& previous_3d_cam,
+                              const TrackletIds& tracklet_ids,
+                              const gtsam::Pose3& previous_pose,
+                              const gtsam::Pose3& motion_model,
+                              const Camera& camera,
+                              const MotionOptions& options)
+{
+  //check that current uv, previous 3d poins and tracklets are all the same size
+  CHECK(current_uv.size() == previous_3d_cam.size() == tracklet_ids.size()) << 
+    "2d-3d and tracklet id correspondences must be the same size!";
+
+  MotionResult result;
+
+  static constexpr size_t kMinPointsRansac = 5; //required for ransac
+  if(current_uv.size() < kMinPointsRansac) {
+    LOG(WARNING) << "Not enoug points to run PnP RASAC with - needed " << kMinPointsRansac;
+    result.success = false;
+    return result;
+  }
 
 
+  std::vector<cv::Point2f> current_2d;
+  std::vector<cv::Point3f> previous_3d;
 
+  for(size_t i = 0; i < current_uv.size(); i++) {
+    current_2d.push_back(current_uv.at(i).pt);
 
+    Landmark lmk_c = previous_3d_cam.at(i);
+    //solve in world frame
+    if(options.solver == MotionOptions::Solver::WORLD) {
+      Landmark lmk_world = previous_pose.transformFrom(lmk_c);
+      previous_3d.push_back(cv::Point3f(static_cast<float>(lmk_world.x()), static_cast<float>(lmk_world.y()),
+                                        static_cast<float>(lmk_world.z())));
+    }
+    else {
+      previous_3d.push_back(cv::Point3f(static_cast<float>(lmk_c.x()), static_cast<float>(lmk_c.y()),
+                                        static_cast<float>(lmk_c.z())));
+    }
+  }
+
+  CHECK(current_2d.size() == previous_3d.size() == tracklet_ids.size());
+  VLOG(2) << "Solving initial camera model with  " << previous_3d.size() << " features";
+
+  const cv::Mat& K = camera.Params().K;
+  const cv::Mat& D = camera.Params().D;
+
+  //solve PnP
+  cv::Mat Rvec(3, 1, CV_64FC1);
+  cv::Mat Tvec(3, 1, CV_64FC1);
+  cv::Mat Rot(3, 3, CV_64FC1);
+  cv::Mat pnp_inliers;  // a [1 x N] vector
+  cv::solvePnPRansac(previous_3d, current_2d, K, D, Rvec, Tvec, false, 
+                     options.pnp_iterations, options.reprojection_error, options.pnp_confidence,
+                     pnp_inliers, cv::SOLVEPNP_AP3P);
+
+  //recover rotation
+  cv::Rodrigues(Rvec, Rot);
+  gtsam::Pose3 pnp_pose = utils::cvMatsToGtsamPose3(Rot, Tvec).inverse();
+
+  if(options.solver == MotionOptions::Solver::CAMERA) {
+    pnp_pose = previous_pose * pnp_pose; //compose to world frame
+  }
+
+  //recover inlier outlier id's from PNP
+  TrackletIds pnp_tracklet_inliers, pnp_tracklet_outliers;
+  for (size_t i = 0; i < pnp_inliers.rows; i++)
+  {
+    int inlier_index = pnp_inliers.at<int>(i);
+    size_t inlier_tracklet = tracklet_ids[inlier_index];
+    pnp_tracklet_inliers.push_back(inlier_tracklet);
+  }
+  //calculate outliers
+  determineOutlierIds(pnp_tracklet_inliers, tracklet_ids, pnp_tracklet_outliers);
+  CHECK_EQ((pnp_tracklet_inliers.size() + pnp_tracklet_outliers.size()), tracklet_ids.size());
+  CHECK_EQ(pnp_tracklet_inliers.size(), pnp_inliers.rows);
+  VLOG(1) << "PNP Inliers/total = " << pnp_inliers.rows << "/" << previous_3d.size();
+  
 }
 
 
-}
+} //tracking_tools
+} //vdo
